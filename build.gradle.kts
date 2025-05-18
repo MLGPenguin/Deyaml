db6999ee0083b4bb6da153adc1806379fd101f12
MLGPenguin <baww123@gmail.com>
2025-05-18 13:55:10 +1000

    init - works with strings

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    kotlin("jvm") version "2.1.20"
+}
+
+group = "com.github.mlgpenguin"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("reflect"))
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(17)
+}
